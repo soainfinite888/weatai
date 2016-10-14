46ef28d6cae6cd32cfa0bb98bf6562f6750e121a
Neil <wer321011@gmail.com>
2016-10-14 05:25:34 -0400

    Left three rubocop offenses. Finished API Library.

--- CWB/lib/weather.rb
+++ CWB/lib/weather.rb
@@ -6,17 +6,17 @@ require 'http'
 credentials = YAML.load(File.read('../config/credentials.yml'))
 file = HTTP.get('http://opendata.cwb.gov.tw/opendataapi',
                 params: { dataid: 'O-A0003-001',
-                         authorizationkey: credentials[:key] })
+                          authorizationkey: credentials[:key] })
 hash = Hash.from_xml(file)
 all_location = {}
-for i in 0..hash['cwbopendata']['location'].length-1
+hash['cwbopendata']['location'].each do |item|
   location = {}
-  place = hash['cwbopendata']['location'][i]['locationName']
-  location['time'] = hash['cwbopendata']['location'][i]['time']['obsTime']
-  location['city'] = hash['cwbopendata']['location'][i]['parameter'][0]['parameterValue']
-  location['town'] = hash['cwbopendata']['location'][i]['parameter'][2]['parameterValue']
-  location['TEMP'] = hash['cwbopendata']['location'][i]['weatherElement'][4]['elementValue']['value']
-  location['HUMD'] = hash['cwbopendata']['location'][i]['weatherElement'][5]['elementValue']['value']
+  place = item['locationName']
+  location['time'] = item['time']['obsTime']
+  location['city'] = item['parameter'][0]['parameterValue']
+  location['town'] = item['parameter'][2]['parameterValue']
+  location['TEMP'] = item['weatherElement'][4]['elementValue']['value']
+  location['HUMD'] = item['weatherElement'][5]['elementValue']['value']
   all_location.store(place, location)
 end
-File.write("../spec/fixtures/data.yml", all_location.to_yaml)
+File.write('../spec/fixtures/data.yml', all_location.to_yaml)
