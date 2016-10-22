1d214d175033db5cd885cfed86e85217ba79a237
chiachienhung <lisa5432126@gmail.com>
2016-10-22 09:55:09 +0800

    Add helper in spec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/rg'
+require 'active_support/all'
+require 'active_support/core_ext/hash'
+require 'yaml'
+require 'http'
+
+require_relative '../lib/cwb.rb'
+require_relative '../lib/cwb_api.rb'
+
+CWB_RESPONSE = YAML.load(File.read('./fixtures/data.yml'))
+CREDENTIALS = YAML.load(File.read('../config/credentials.yml'))
